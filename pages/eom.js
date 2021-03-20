193f60418015706a140710f43484a31feb6cb961
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-03-19 22:34:25 -0700

    init

---
+++ pages/eom.js
@@ -0,0 +1,36 @@
+import styles from '../styles/EOM.module.CSS';
+import { Toolbar } from '../components/toolbar';
+
+export const EOM = ({ employee }) => {
+    console.log(employee);
+    return(
+    <div className="page-container">
+        <Toolbar />
+        <div class={styles.main}>
+            <h1>Employee of the Month</h1>
+
+            <div className={styles.employeeOfTheMonth}>
+                <h3>{employee.name}</h3>
+                <h6>{employee.position}</h6>
+                <img src={employee.image} />
+                <p>{employee.description}</p>
+            </div>
+        </div>
+    </div>
+);
+};
+
+export const getServerSideProps = async pageContext => {
+    const apiResponse = await fetch(
+        'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth',
+      );
+      const employee = await apiResponse.json();
+
+      return{
+          props: {
+              employee
+          }
+      };
+};
+
+export default EOM;
